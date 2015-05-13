cd44ba0f2cb2b1f82c2bcc3eafab32c6ddde9bb4
Matthew Lewis <matt@mplewis.com>
2015-05-13 16:07:46 -0500

    More progress

--- digikey.py
+++ digikey.py
@@ -6,22 +6,44 @@ Digi-Key terminal search engine.
 Usage:
     digikey KEYWORDS...
     digikey --category CAT_NAME KEYWORDS...
+    digikey --clear-cache
 
 Options:
     --category CAT_NAME     Search for products in a specific Digi-Key category,
                             e.g. "Battery Chargers", "led discrete".
                             Supports fuzzy search.
+    --clear-cache           Clears local data cached from Digi-Key. Data expires
+                            on its own every 15 minutes, even if not cleared
+                            manually.
 """
 
 import requests
+import appdirs
 from bs4 import BeautifulSoup
 from docopt import docopt
 from fuzzywuzzy import process
 from getch import getch
 from colored import fg, attr
+from beaker.cache import CacheManager
+from beaker.util import parse_cache_config_options
 
 import re
 import sys
+
+
+APP_NAME = 'digiglass'
+APP_AUTHOR = 'mplewis'
+MAX_CATEGORIES = 20
+
+CACHE_OPTS = {
+    'cache.type': 'file',
+    'cache.data_dir': appdirs.user_cache_dir(APP_NAME, APP_AUTHOR) + '/data',
+    'cache.lock_dir': appdirs.user_cache_dir(APP_NAME, APP_AUTHOR) + '/lock',
+    'cache.expire': 900  # 15 minutes
+}
+CACHE_MANAGER = CacheManager(**parse_cache_config_options(CACHE_OPTS))
+CATEGORY_CACHE = CACHE_MANAGER.get_cache('category')
+LETTERS = 'abcdefghijklmnopqrstuvwxyz'[:MAX_CATEGORIES]
 
 
 class Category:
@@ -66,10 +88,29 @@ def parse_categories(page_tree):
     return cat_items
 
 
-def all_categories():
+def _all_categories():
     cats_tree = get_as_bs_tree('http://www.digikey.com/product-search/en')
     categories = parse_categories(cats_tree)
     return categories
+
+
+def all_categories():
+    return CATEGORY_CACHE.get(key='all_categories', createfunc=_all_categories)
+
+
+def _categories_for_keyword(keyword):
+    params = {'keywords': keyword}
+    cats_tree = get_as_bs_tree('http://www.digikey.com/product-search/en',
+                               params=params)
+    categories = parse_categories(cats_tree)
+    categories.sort(key=lambda c: c.qty, reverse=True)
+    return categories
+
+
+def categories_for_keyword(keyword):
+    key = 'category.{}'.format(keyword)
+    to_call = lambda: _categories_for_keyword(keyword)
+    return CATEGORY_CACHE.get(key=key, createfunc=to_call)
 
 
 def get_as_bs_tree(*args, **kwargs):
@@ -80,10 +121,20 @@ def get_as_bs_tree(*args, **kwargs):
     return BeautifulSoup(requests.get(*args, **kwargs).text)
 
 
-def closest_categories(search_term, categories, limit=10):
-    return [cat for (cat, score) in
-            process.extract(search_term, categories,
-                            processor=lambda c: c.name, limit=limit)]
+def _category_fuzzy_name(category):
+    """
+    Strip non-alphanumerics from a category parent and name and lowercase it for
+    use with fuzzy search.
+    """
+    hyphenated = '{} - {}'.format(category.parent, category.name)
+    alphanumeric_only = re.sub(r'\W+', ' ', hyphenated)
+    return alphanumeric_only.lower()
+
+
+def closest_categories(search_term, categories, limit=MAX_CATEGORIES):
+    results = process.extract(search_term, categories,
+                              processor=_category_fuzzy_name, limit=limit)
+    return [category for (category, _) in results[:limit]]
 
 
 def getkey(message):
@@ -95,23 +146,52 @@ def getkey(message):
 
 
 def get_user_category(categories):
-    letters = 'abcdefghijklmnopqrstuvwxyz'[:len(categories)]
-    lookup = dict(zip(letters, categories))
+    lookup = dict(zip(LETTERS, categories))
+    longest = 0
+    for category in lookup.values():
+        digits = len(str(category.qty))
+        if digits > longest:
+            longest = digits
     for letter, category in sorted(lookup.items(), key=lambda t: t[0]):
-        print('{}{}{}. {}{}{} - {}{}{}'.format(
+        print('{}{}{}. ({}{}{}) {}{}{} - {}{}{}'.format(
               fg('red'), letter, attr('reset'),
+              fg('blue'), str(category.qty).rjust(longest), attr('reset'),
               fg('green'), category.parent, attr('reset'),
               fg('yellow'), category.name, attr('reset')))
-    key = getkey('Select a category')
-    return lookup[key.lower()]
+    found = None
+    while not found:
+        try:
+            key = getkey('Select a category')
+            found = lookup[key.lower()]
+        except KeyError:
+            print('Invalid selection.')
+    return found
 
-if __name__ == '__main__':
+
+def main():
     args = docopt(__doc__)
+
+    clear_cache = args['--clear-cache']
     dirty_cat = args['--category']
     search_term = ' '.join(args['KEYWORDS'])
+
+    if clear_cache:
+        CATEGORY_CACHE.clear()
+        print('Cache cleared.')
+        return
+
     if dirty_cat:
-        suggested_cats = closest_categories(search_term, all_categories())
+        # Search with category
+        suggested_cats = closest_categories(dirty_cat, all_categories())
         clean_cat = get_user_category(suggested_cats)
         print(clean_cat)
+
     else:
-        print('Searching for {}, no category given'.format(search_term))
+        # Pick categories from search keyword
+        suggested_cats = categories_for_keyword(search_term)
+        clean_cat = get_user_category(suggested_cats)
+        print(clean_cat)
+
+
+if __name__ == '__main__':
+    main()
